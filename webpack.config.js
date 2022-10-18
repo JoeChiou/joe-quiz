ec2357203a9b86b6e936caf3ea364fe58e1281e8
JoeChiou <iamasheep2008@gmail.com>
2022-10-18 16:16:00 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development", 
+  entry: "./src/index.tsx", 
+  output: {
+    path: path.resolve(__dirname, "dist"), 
+    filename: "bundle.js", 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, 
+        loader: "ts-loader", 
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+    new CleanWebpackPlugin({}),
+  ],
+};
